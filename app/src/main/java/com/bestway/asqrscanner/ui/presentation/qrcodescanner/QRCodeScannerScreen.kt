74e658d56f8614a22d261efabda7fd4cb7c2ceb1
anmolsinghsahi <anmol.sahi@mutualmobile.com>
2023-03-02 13:59:22 +0530

    add in app updates

--- app/src/main/java/com/bestway/asqrscanner/ui/presentation/qrcodescanner/QRCodeScannerScreen.kt
+++ app/src/main/java/com/bestway/asqrscanner/ui/presentation/qrcodescanner/QRCodeScannerScreen.kt
@@ -15,10 +15,23 @@ import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,7 +44,6 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.Dialog
 import androidx.core.content.ContextCompat
 import com.bestway.asqrscanner.R
-import com.bestway.asqrscanner.data.ReviewManager
 import com.bestway.asqrscanner.ui.presentation.QRCodeBackground
 import com.google.mlkit.vision.barcode.BarcodeScanner
 import com.google.mlkit.vision.common.InputImage
@@ -43,7 +55,7 @@ import java.io.IOException
 fun QRCodeScannerScreen(
     barcodeScanner: BarcodeScanner
 ) {
-    var isTorchEnable by remember { mutableStateOf(false) }
+    var isTorchEnable by rememberSaveable { mutableStateOf(false) }
     var camera: Camera? by remember { mutableStateOf<Camera?>(null) }
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
@@ -51,15 +63,15 @@ fun QRCodeScannerScreen(
     var scannedResult by rememberSaveable { mutableStateOf("") }
 
     var selectedImageScannedResult by rememberSaveable { mutableStateOf("") }
-    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
-    var showLoading by remember { mutableStateOf(false) }
+    var selectedImageUri by rememberSaveable { mutableStateOf<Uri?>(null) }
+    var showLoading by rememberSaveable { mutableStateOf(false) }
     var inputImage by remember { mutableStateOf<InputImage?>(null) }
 
     val cameraProviderFuture = remember {
         ProcessCameraProvider.getInstance(context)
     }
 
-    var hasCameraPermission by remember {
+    var hasCameraPermission by rememberSaveable {
         mutableStateOf(
             ContextCompat.checkSelfPermission(
                 context,
